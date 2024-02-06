7a2e63631a04d13e0cab3c9c559257374b0c3ed8
ading01 <allan.ding@yale.edu>
2024-02-06 17:12:11 -0500

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "./contexts/ThemeContext";
 // import { SpeedInsights } from "@vercel/speed-insights/next";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 // import logo from "./logo.svg";
 import {
@@ -20,6 +21,8 @@ import Navbar from "./components/Navbar"; // Adjust the import path based on you
 const App: React.FC = () => {
   return (
     <ThemeProvider>
+      <SpeedInsights />
+      <Analytics />
       <GlobalStyle />
       <Router>
         <Navbar />
